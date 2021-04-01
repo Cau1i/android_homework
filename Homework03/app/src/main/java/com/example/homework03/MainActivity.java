8c01f061443165566bfe7520d178fcea2761d7fe
HeYiCheng1X <1424864061@qq.com>
2021-04-01 18:47:36 +0800

    创建新版对话框

--- Homework03/app/src/main/java/com/example/homework03/MainActivity.java
+++ Homework03/app/src/main/java/com/example/homework03/MainActivity.java
@@ -9,30 +9,34 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void onOldButtonClicked(View view){
+    public void onOldButtonClicked(View view) {
         AlertDialog dialog;
         dialog = new AlertDialog.Builder(this).setTitle("旧版对话框")
                 .setMessage("这是旧版的对话框，使用简单\n但是没有自动支持屏幕翻转！")
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(),"你确定了！",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "你确定了！", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .setNegativeButton("取消", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(),"你取消了！",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "你取消了！", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .create();
-                dialog.show();
+        dialog.show();
+    }
+
+    public void onSimpleButtonClicked(View view){
+        SimpleDialogFragment dialog = new SimpleDialogFragment();
+        dialog.show(getFragmentManager(),"simple");
     }
 }
