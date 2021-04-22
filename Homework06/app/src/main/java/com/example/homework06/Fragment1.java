d20c0ea9cd4a5c35fa9d11aae489d584843f4215
HeYiCheng1X <1424864061@qq.com>
2021-04-22 19:09:39 +0800

    将导航的两个界面更改为实验2、实验4中的部分界面

--- Homework06/app/src/main/java/com/example/homework06/Fragment1.java
+++ Homework06/app/src/main/java/com/example/homework06/Fragment1.java
@@ -3,7 +3,7 @@ package com.example.homework06;
 import androidx.fragment.app.Fragment;
 
 public class Fragment1 extends Fragment {
-    public Fragment1(){
+    public Fragment1() {
         super(R.layout.fragment1);
     }
 }
